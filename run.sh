02b563f186a64702cbcdf65c0b427b639991ab20
Daniela Puiu <dpuiu@jhu.edu>
2026-09-22 18:14:29 -0400

    first commit

---
+++ run.sh
@@ -0,0 +1,54 @@
+npm create astro@latest -- --template starlight
+cd astrotest
+
+npm run dev   #http://localhost:4321/
+
+npm run build ; python -m http.server 4321 --bind 127.0.0.1 -d ./dist
+
+##########
+
+git clone https://github.com/withastro/starlight.git
+#corepack enable
+#corepack prepare pnpm@latest --activate
+#pnpm approve-builds
+#pnpm install
+#pnpm dlx @astrojs/upgrade
+cd starlight/examples/tailwind
+npm run dev
+
+##########
+
+sed -i '/^(/d' *md
+yq ".people" ~/www.ccb.jhu.edu/_people/faculty.yaml > src/data/faculty.json 
+yq '.software' ~/www.ccb.jhu.edu/_software/all.yaml > src/data/software.json
+
+########
+
+#src/layouts/Layout.astro
+#pm install bootstrap
+
+jq '.[].role' src/data/people.json  -r| sort -u
+
+sed 's/faculty/alumni/g; s/Faculty/Alumni/g' src/content/docs/people/faculty.mdx  > src/content/docs/people/alumni.mdx
+sed 's/faculty/collaborators/g; s/Faculty/Collaborators/g' src/content/docs/people/faculty.mdx  > src/content/docs/people/collaborators.mdx
+sed 's/faculty/postdocs/g; s/Faculty/Postdocs/g' src/content/docs/people/faculty.mdx  > src/content/docs/people/postdocs.mdx
+sed 's/faculty/staff/g; s/Faculty/Staff/g' src/content/docs/people/faculty.mdx  > src/content/docs/people/staff.mdx
+sed 's/faculty/students/g; s/Faculty/Students/g' src/content/docs/people/faculty.mdx  > src/content/docs/people/students.mdx
+
+###
+
+jq '.[].category' src/data/software.json  -r| sort -u
+  "alignment"
+  "gene-finding"
+  "genome-assembly"
+  "metagenomics"
+  "other-tools"
+  "transcriptome-assembly"
+  "variant-analysis"
+
+sed 's/alignment/gene-finding/g; s/Alignment/Gene-finding/g' src/content/docs/software/alignment.mdx   >  src/content/docs/software/gene-finding.mdx
+sed 's/alignment/genome-assembly/g; s/Alignment/Genome-assembly/g' src/content/docs/software/alignment.mdx   >  src/content/docs/software/genome-assembly.mdx
+sed 's/alignment/metagenomics/g; s/Alignment/Metagenomics/g' src/content/docs/software/alignment.mdx   >  src/content/docs/software/metagenomics.mdx
+sed 's/alignment/other-tools/g; s/Alignment/Other-tools/g' src/content/docs/software/alignment.mdx   >  src/content/docs/software/other-tools.mdx
+sed 's/alignment/transcriptome-assembly/g; s/Alignment/Transcriptome-assembly/g' src/content/docs/software/alignment.mdx   >  src/content/docs/software/transcriptome-assembly.mdx
+sed 's/alignment/variant-analysis/g; s/Alignment/Variant-analysis/g' src/content/docs/software/alignment.mdx   >  src/content/docs/software/variant-analysis.mdx
---
+++ src/data/substitutions.js
@@ -0,0 +1,168 @@
+export const substitutions = {
+  arch: {
+    text: "Advanced Research Computing at Hopkins",
+    url: "https://www.arch.jhu.edu/"
+  },
+  ARCH: {
+    text: "ARCH",
+    url: "https://www.arch.jhu.edu/"
+  },
+
+  bio: {
+    text: "Department of Biology",
+    url: "https://bio.jhu.edu/"
+  },
+
+  bme: {
+    text: "Department of Biomedical Engineering",
+    url: "https://www.bme.jhu.edu/"
+  },
+  BME: {
+    text: "BME",
+    url: "https://www.bme.jhu.edu/"
+  },
+
+  bs: {
+    text: "Department of Biostatistics",
+    url: "https://publichealth.jhu.edu/departments/biostatistics"
+  },
+  BS: {
+    text: "BS",
+    url: "https://publichealth.jhu.edu/departments/biostatistics"
+  },
+
+  cardio: {
+    text: "Department of Medicine, Division of Cardiology",
+    url: "https://www.hopkinsmedicine.org/heart-vascular-institute/cardiology"
+  },
+
+  cbcc: {
+    text: "Computational Biology Consulting Core",
+    url: null
+  },
+
+  ccb: {
+    text: "Center for Computational Biology",
+    url: "https://ccb.jhu.edu/"
+  },
+  CCB: {
+    text: "CCB",
+    url: "https://ccb.jhu.edu/"
+  },
+
+  chembe: {
+    text: "Department of Chemical & Biomolecular Engineering",
+    url: "https://engineering.jhu.edu/chembe/"
+  },
+
+  cs: {
+    text: "Department of Computer Science",
+    url: "https://www.cs.jhu.edu/"
+  },
+  CS: {
+    text: "CS",
+    url: "https://www.cs.jhu.edu/"
+  },
+
+  compbio: {
+    text: "Computational Biology & Medicine",
+    url: "https://www.cs.jhu.edu/research/computational-biology-medicine/"
+  },
+
+  dhpm: {
+    text: "Department of Health Policy and Management",
+    url: "https://publichealth.jhu.edu/departments/health-policy-and-management"
+  },
+
+  dsai: {
+    text: "Data Science & AI Institute",
+    url: "https://ai.jhu.edu/"
+  },
+  DSAI: {
+    text: "DSAI",
+    url: "https://ai.jhu.edu/"
+  },
+
+  dgm: {
+    text: "Department of Genetic Medicine",
+    url: "https://www.hopkinsmedicine.org/genetic-medicine/"
+  },
+  DGM: {
+    text: "DGM",
+    url: "https://www.hopkinsmedicine.org/genetic-medicine/"
+  },
+
+  icm: {
+    text: "Institute for Computational Medicine",
+    url: "https://icm.jhu.edu/"
+  },
+
+  idies: {
+    text: "Institute for Data-Intensive Engineering and Science",
+    url: "https://www.idies.jhu.edu/"
+  },
+  IDES: {
+    text: "IDES",
+    url: "https://www.idies.jhu.edu/"
+  },
+
+  jhu: {
+    text: "Johns Hopkins University",
+    url: "https://www.jhu.edu/"
+  },
+  JHU: {
+    text: "JHU",
+    url: "https://www.jhu.edu/"
+  },
+
+  ksas: {
+    text: "Krieger School of Arts and Sciences",
+    url: "https://krieger.jhu.edu/"
+  },
+  KSAS: {
+    text: "KSAS",
+    url: "https://krieger.jhu.edu/"
+  },
+
+  mc: {
+    text: "Malone Center for Engineering in Healthcare",
+    url: "https://malonecenter.jhu.edu/"
+  },
+
+  med: {
+    text: "Department of Medicine",
+    url: "https://www.hopkinsmedicine.org/medicine"
+  },
+
+  skcc: {
+    text: "The Sidney Kimmel Comprehensive Cancer Center",
+    url: "https://www.hopkinsmedicine.org/kimmel-cancer-center"
+  },
+
+  som: {
+    text: "School of Medicine",
+    url: "https://www.hopkinsmedicine.org/som"
+  },
+  SOM: {
+    text: "SOM",
+    url: "https://www.hopkinsmedicine.org/som"
+  },
+
+  sph: {
+    text: "Bloomberg School of Public Health",
+    url: "https://publichealth.jhu.edu/"
+  },
+  SPH: {
+    text: "SPH",
+    url: "https://publichealth.jhu.edu/"
+  },
+
+  wse: {
+    text: "Whiting School of Engineering",
+    url: "https://engineering.jhu.edu/"
+  },
+  WSE: {
+    text: "WSE",
+    url: "https://engineering.jhu.edu/"
+  }
+};
