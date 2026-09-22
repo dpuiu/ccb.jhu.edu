e95b6ccf4c1db430dfc68636152a1b346955ba8b
Daniela Puiu <dpuiu@jhu.edu>
2026-09-22 18:40:12 -0400

    updated astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,3 +1,6 @@
+import { defineConfig } from 'astro/config';
+import starlight from '@astrojs/starlight';
+
 export default defineConfig({
   site: 'https://dpuiu.github.io',
   base: '/ccb.jhu.edu',
