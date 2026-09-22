2c6264dfaeecf6bf1bb5b1b71a27fd54c071960e
Daniela Puiu <dpuiu@jhu.edu>
2026-09-22 18:30:25 -0400

    updated astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,23 +1,29 @@
-// @ts-check
-import { defineConfig } from 'astro/config';
-import starlight from '@astrojs/starlight';
-
-// https://astro.build/config
 export default defineConfig({
-	integrations: [
-		starlight({
-			title: 'CCB',
-			social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/withastro/starlight' }],
-			sidebar: [
-				{
-					label: 'People',
-					items: [{ autogenerate: { directory: 'people' } }],
-				},
-				{
-					label: 'Software',
-					items: [{ autogenerate: { directory: 'software' } }],
-				},
-			],
-		}),
-	],
+  site: 'https://dpuiu.github.io',
+  base: '/ccb.jhu.edu',
+
+  integrations: [
+    starlight({
+      title: 'CCB',
+
+      social: [
+        {
+          icon: 'github',
+          label: 'GitHub',
+          href: 'https://github.com/dpuiu/www.ccb.jhu.edu',
+        },
+      ],
+
+      sidebar: [
+        {
+          label: 'People',
+          items: [{ autogenerate: { directory: 'people' } }],
+        },
+        {
+          label: 'Software',
+          items: [{ autogenerate: { directory: 'software' } }],
+        },
+      ],
+    }),
+  ],
 });
